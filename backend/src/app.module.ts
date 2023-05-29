27b2d911f035769f65fd402d99bef139e3322a1b
zufarhandito <zufarhandito@gmail.com>
2023-05-29 19:16:06 +0700

    remove duplicate usersmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { MasterModule } from './master/master.module';
 import { CurriculumModule } from './curriculum/curriculum.module';
 import { PlacementModule } from './placement/placement.module';
