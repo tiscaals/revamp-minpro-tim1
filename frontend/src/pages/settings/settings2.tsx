1bc461d9fc6bfdcce94be5d8f8631144553a244a
Tamariska Laras Suci <Tamariska Laras Suci>
2023-06-05 03:57:16 +0700

    add kerangka frontend

---
+++ frontend/src/pages/settings/settings2.tsx
@@ -0,0 +1,13 @@
+import BreadcrumbsSlice from "../shared/breadcrumbs"
+
+
+const Settings = () => {
+    return (
+        <>
+            <BreadcrumbsSlice/>
+            <h1 className="text-black">INI HALAMAN SETTING 2</h1>
+        </>
+    )
+}
+
+export default Settings
