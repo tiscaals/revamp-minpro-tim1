b477e6724e8d5182b9b30f20182c1fa3ceee4186
zufarhandito <zufarhandito@gmail.com>
2023-06-06 01:38:51 +0700

    update kerangka

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 import Layout from './shared/layout';
+import { Input, ThemeProvider } from '@material-tailwind/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    // <div>
+    //   <Input label='asasd'/>
+    // </div>
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
--- frontend/src/pages/shared/sidebar.tsx
+++ frontend/src/pages/shared/sidebar.tsx
@@ -1,60 +1,126 @@
-import { forwardRef, LegacyRef, useState } from "react";
-import Link from 'next/link'
-import Image from 'next/image'
-import { useRouter } from "next/router";
-import logo from "../../images/codexlogo.png";
-import { MdCottage, MdGroup, MdShopTwo, MdCategory, MdViewModule, MdLocationOn, MdSportsMartialArts, MdGroups, MdAssignment, MdPostAdd, MdLibraryBooks, MdGroupAdd } from 'react-icons/md'
-import { useEffect } from "react";
-import jwt from 'jsonwebtoken'
-
+import { forwardRef, LegacyRef, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import logo from '../../images/codexlogo.png';
+import {
+  MdCottage,
+  MdGroup,
+  MdShopTwo,
+  MdCategory,
+  MdViewModule,
+  MdLocationOn,
+  MdSportsMartialArts,
+  MdGroups,
+  MdAssignment,
+  MdPostAdd,
+  MdLibraryBooks,
+  MdGroupAdd,
+} from 'react-icons/md';
+import { useEffect } from 'react';
 
 const SideBar = forwardRef(({}, ref: LegacyRef<HTMLDivElement>) => {
-  const [listMenu, setListMenu]=useState([{ to: '/', path: '/', icon:<MdCottage/>, name:'Home'}])
-  let token:any;
-  useEffect(()=>{
-    token = localStorage.getItem('AuthToken')
+  const [listMenu, setListMenu] = useState([
+    { to: '/', path: '/', icon: <MdCottage />, name: 'Home' },
+  ]);
+  let token: any;
+  useEffect(() => {
+    token = localStorage.getItem('AuthToken');
     // const decoded:any = jwt.verify(token, `SECRETKEY`);
-    const decoded = {role_id:5,username:"Tamariska"} //INI DUMMY NTAR HAPUS AJA AKTIFIN YG ATAS
+    const decoded = { role_id: 5, username: 'Tamariska' }; //INI DUMMY NTAR HAPUS AJA AKTIFIN YG ATAS
     switch (decoded.role_id) {
       //admin
       case 1:
         setListMenu([
-          { to: '/', path: '/', icon:<MdCottage/>, name:'Home'},
-          { to: '/category', path: '/category', icon:<MdCategory/>, name:'Category'},
-          { to: '/skill', path: '/skill', icon:<MdSportsMartialArts/>, name:'Skill'},
-          { to: '/modules', path: '/modules', icon:<MdViewModule/>, name:'Modules'},
-          { to: '/locations', path: '/locations', icon:<MdLocationOn/>, name:'Locations'}
-          ])
+          { to: '/', path: '/', icon: <MdCottage />, name: 'Home' },
+          {
+            to: '/category',
+            path: '/category',
+            icon: <MdCategory />,
+            name: 'Category',
+          },
+          {
+            to: '/skill',
+            path: '/skill',
+            icon: <MdSportsMartialArts />,
+            name: 'Skill',
+          },
+          {
+            to: '/modules',
+            path: '/modules',
+            icon: <MdViewModule />,
+            name: 'Modules',
+          },
+          {
+            to: '/locations',
+            path: '/locations',
+            icon: <MdLocationOn />,
+            name: 'Locations',
+          },
+        ]);
         break;
 
       //direksi/busdev
       case 2:
-        setListMenu([ //MENU BLM TAU
-          { to: '/', path: '/', icon:<MdCottage/>, name:'Home'},
-          { to: '/user', path: '/user', icon:<MdGroup/>, name:'User'},
-          { to: '/category', path: '/category', icon:<MdCategory/>, name:'Category'},
-          { to: '/product', path: '/product', icon:<MdShopTwo/>, name:'Product'}
-          ])
+        setListMenu([
+          //MENU BLM TAU
+          { to: '/', path: '/', icon: <MdCottage />, name: 'Home' },
+          { to: '/user', path: '/user', icon: <MdGroup />, name: 'User' },
+          {
+            to: '/category',
+            path: '/category',
+            icon: <MdCategory />,
+            name: 'Category',
+          },
+          {
+            to: '/product',
+            path: '/product',
+            icon: <MdShopTwo />,
+            name: 'Product',
+          },
+        ]);
         break;
 
       //hr
       case 3:
-        setListMenu([ //MENU BLM TAU
-          { to: '/', path: '/', icon:<MdCottage/>, name:'Home'},
-          { to: '/user', path: '/user', icon:<MdGroup/>, name:'User'},
-          { to: '/category', path: '/category', icon:<MdCategory/>, name:'Category'},
-          { to: '/product', path: '/product', icon:<MdShopTwo/>, name:'Product'}
-          ])
+        setListMenu([
+          //MENU BLM TAU
+          { to: '/', path: '/', icon: <MdCottage />, name: 'Home' },
+          { to: '/user', path: '/user', icon: <MdGroup />, name: 'User' },
+          {
+            to: '/category',
+            path: '/category',
+            icon: <MdCategory />,
+            name: 'Category',
+          },
+          {
+            to: '/product',
+            path: '/product',
+            icon: <MdShopTwo />,
+            name: 'Product',
+          },
+        ]);
         break;
 
       //talent
-      case 4: 
-        setListMenu([ //MENU BLM TAU
-          { to: '/', path: '/', icon:<MdCottage/>, name:'Home'},
-          { to: '/user', path: '/user', icon:<MdGroup/>, name:'User'},
-          { to: '/category', path: '/category', icon:<MdCategory/>, name:'Category'},
-          { to: '/product', path: '/product', icon:<MdShopTwo/>, name:'Product'}
-          ])
+      case 4:
+        setListMenu([
+          //MENU BLM TAU
+          { to: '/', path: '/', icon: <MdCottage />, name: 'Home' },
+          { to: '/user', path: '/user', icon: <MdGroup />, name: 'User' },
+          {
+            to: '/category',
+            path: '/category',
+            icon: <MdCategory />,
+            name: 'Category',
+          },
+          {
+            to: '/product',
+            path: '/product',
+            icon: <MdShopTwo />,
+            name: 'Product',
+          },
+        ]);
         break;
 
       //trainer
@@ -62,59 +128,88 @@ const SideBar = forwardRef(({}, ref: LegacyRef<HTMLDivElement>) => {
       //recruiter
       case 6:
         setListMenu([
-          { to: '/', path: '/', icon:<MdCottage/>, name:'Home'},
-          { to: '/candidate', path: '/candidate', icon:<MdGroupAdd/>, name:'Candidate'},
-          { to: '/batch', path: '/batch', icon:<MdCategory/>, name:'Batch'},
-          { to: '/talents', path: '/talents', icon:<MdGroups/>, name:'Talents'},
-          { to: '/curriculum', path: '/curriculum', icon:<MdLibraryBooks/>, name:'Curriculum'},
-          { to: '/assignment', path: '/assignment', icon:<MdAssignment/>, name:'Assignment'},
-          { to: '/postinghiring', path: '/postinghiring', icon:<MdPostAdd/>, name:'Posting Hiring'}
-          ])
+          { to: '/', path: '/', icon: <MdCottage />, name: 'Home' },
+          {
+            to: '/candidate',
+            path: '/candidate',
+            icon: <MdGroupAdd />,
+            name: 'Candidate',
+          },
+          { to: '/batch', path: '/batch', icon: <MdCategory />, name: 'Batch' },
+          {
+            to: '/talents',
+            path: '/talents',
+            icon: <MdGroups />,
+            name: 'Talents',
+          },
+          {
+            to: '/curriculum',
+            path: '/curriculum',
+            icon: <MdLibraryBooks />,
+            name: 'Curriculum',
+          },
+          {
+            to: '/assignment',
+            path: '/assignment',
+            icon: <MdAssignment />,
+            name: 'Assignment',
+          },
+          {
+            to: '/postinghiring',
+            path: '/postinghiring',
+            icon: <MdPostAdd />,
+            name: 'Posting Hiring',
+          },
+          {
+            to: '/bootcamp',
+            path: '/bootcamp',
+            icon: <MdPostAdd />,
+            name: 'Bootcamp',
+          },
+        ]);
         break;
       default:
         setListMenu([
-          { to: '/', path: '/', icon:<MdCottage/>, name:'Home'}
-          ])
+          { to: '/', path: '/', icon: <MdCottage />, name: 'Home' },
+        ]);
         break;
     }
-      
-    console.log(listMenu);
-  },[])
-    const router = useRouter()
-  
+
+    // console.log(listMenu);
+  }, []);
+  const router = useRouter();
+
   return (
-    <div ref={ref} className="fixed w-56 h-full bg-slate-900 shadow-sm z-10 py-4 px-6">
+    <div
+      ref={ref}
+      className="fixed w-56 h-full bg-teal-600 shadow-sm z-10 py-4 px-6"
+    >
       <div className="flex justify-center mt-4 mb-4">
-          <Image
-            className="w-full h-auto"
-            src={logo}
-            alt="company logo"
-          />
+        <Image className="w-full h-auto" src={logo} alt="company logo" />
       </div>
 
       <div className="md:flex-col md:min-w-full flex flex-col list-none">
-        {(listMenu || []).map((mn)=>(
+        {(listMenu || []).map(mn => (
           <Link href={`${mn.to}`}>
-              <div
-                  className={`rounded-sm px-3 py-3 text-xs uppercase font-bold flex items-center ${router.pathname == mn.path
-                      ? "bg-sky-100 text-sky-600 hover:text-sky-800"
-                      : "text-slate-500 hover:text-slate-400"
-                  }`}
-              >
-                  <div className="mr-2">
-                      {mn.icon}
-                  </div>
-                  <div>
-                      <p>{mn.name}</p>
-                  </div>
+            <div
+              className={`rounded-sm px-3 py-3 text-xs uppercase font-bold flex items-center ${
+                router.pathname == mn.path
+                  ? 'bg-sky-100 text-sky-600 hover:text-sky-800'
+                  : 'text-slate-500 hover:text-slate-400'
+              }`}
+            >
+              <div className="mr-2">{mn.icon}</div>
+              <div>
+                <p>{mn.name}</p>
               </div>
-          </Link>)
-        )}
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
 });
 
-SideBar.displayName = "SideBar";
+SideBar.displayName = 'SideBar';
 
 export default SideBar;
