91fa02f0dd707c9afc6e95294a10d8b2a0c9d7a1
Rahmi Izzati <63924892+RIzzati10@users.noreply.github.com>
2023-06-06 12:07:58 +0700

    Update placement.sql
    -nyamain sama penggabungan 1
    -enum ganti ke check
    -account manager reference ke hr.employee

--- sql/placement.sql
+++ sql/placement.sql
@@ -1,25 +1,20 @@
 create schema hr;
-create type hr.emp_marital_status as enum ('M', 'S');
-create type hr.emp_gender as enum ('M', 'F');
-create type hr.emp_salaried_flag as enum ('0', '1');
-create type hr.emp_current_flag as enum ('0', '1');
-create type hr.emp_type as enum ('internal', 'outsource');
 
 create table hr.employee(
-	emp_entity_id integer primary key,
+	emp_entity_id integer primary key not null,
 	emp_emp_number varchar(25) unique,
 	emp_national_id varchar(25) unique,
-	emp_birth_date date,
-	emp_marital_status hr.emp_marital_status,
-	emp_gender hr.emp_gender,
-	emp_hire_date timestamptz,
-	emp_salaried_flag hr.emp_salaried_flag,
+	emp_birth_date date not null,
+	emp_marital_status varchar(15) check (emp_marital_status in('M', 'S')),
+	emp_gender varchar(15) check (emp_gender in ('M', 'F')),
+	emp_hire_date timestamp with time zone,
+	emp_salaried_flag varchar(15) check (emp_salaried_flag in ('0', '1')),
 	emp_vacation_hours smallint,
 	emp_sickleave_hours smallint,
-	emp_current_flag hr.emp_current_flag,
+	emp_current_flag varchar(15) check(emp_current_flag in ('0', '1')),
 	emp_emp_entity_id integer,
-	emp_modified_date timestamptz,
-	emp_type hr.emp_type,
+	emp_modified_date timestamp with time zone,
+	emp_type varchar(15) check(emp_type in ('internal', 'outsource')),
 	constraint fk_emp_entity_id
 	foreign key(emp_emp_entity_id)
 	references hr.employee(emp_entity_id)
@@ -31,28 +26,27 @@ add constraint fk_user_entity_id
 foreign key(emp_entity_id)
 references users.users(user_entity_id);
 
-create type hr.ephi_pay_frequence as enum ('1', '2');
-
 create table hr.employee_pay_history(
-	ephi_entity_id integer,
-	ephi_rate_change_date timestamptz,
+	ephi_entity_id integer not null,
+	ephi_rate_change_date timestamp with time zone not null,
 	ephi_rate_salary numeric,
-	ephi_modified_date timestamptz,
+	ephi_pay_frequence varchar(15) check (ephi_pay_frequence in ('1', '2')),
+	ephi_modified_date timestamp with time zone,
 	primary key (ephi_entity_id, ephi_rate_change_date)
 )
 
 create table hr.department(
 	dept_id serial primary key,
 	dept_name varchar(50) unique,
-	dept_modified_date timestamptz
+	dept_modified_date timestamp with time zone
 )
 
 create table hr.employee_department_history(
-	edhi_id serial,
-	edhi_entity_id integer,
-	edhi_start_date timestamptz,
-	edhi_end_date timestamptz,
-	edhi_modified_date timestamptz,
+	edhi_id serial not null,
+	edhi_entity_id integer not null,
+	edhi_start_date timestamp with time zone,
+	edhi_end_date timestamp with time zone,
+	edhi_modified_date timestamp with time zone,
 	edhi_dept_id integer,
 	constraint fk_edhi_entity_id
 	foreign key(edhi_entity_id)
@@ -66,21 +60,25 @@ create table hr.employee_department_history(
 create type hr.ecco_status as enum ('onsite', 'online', 'hybrid');
 
 create table hr.employee_client_contract(
-	ecco_id serial,
-	ecco_entity_id integer,
+	ecco_id serial not null,
+	ecco_entity_id integer not null,
 	ecco_contract_no varchar(55),
-	ecco_start_date timestamptz,
-	ecco_end_date timestamptz,
+	ecco_contract_date timestamp with time zone,
+	ecco_start_date timestamp with time zone,
+	ecco_end_date timestamp with time zone,
 	ecco_notes varchar(512),
-	ecco_modified_date timestamptz,
+	ecco_modified_date timestamp with time zone,
 	ecco_media_link varchar(255),
-	ecco_status hr.ecco_status,
+	ecco_status varchar(15) check (ecco_status in ('onsite', 'online', 'hybrid')) ,
 	ecco_joty_id integer,
 	ecco_account_manager integer,
 	ecco_clit_id integer,
 	constraint fk_ecco_entity_id
 	foreign key (ecco_entity_id)
 	references hr.employee(emp_entity_id),
+	constraint fk_ecco_account_manager
+	foreign key (ecco_account_manager)
+	references hr.employee(emp_joro_id),
 	primary key(ecco_id, ecco_entity_id)
 );
 
@@ -89,12 +87,6 @@ alter table hr.employee_client_contract
 add constraint fk_ecco_joty_id
 foreign key (ecco_joty_id)
 references master.job_type(joty_id);
-
--- relasi ke 
-alter table hr.employee_client_contract
-add constraint fk_ecco_account_manager
-foreign key (ecco_account_manager)
-references x(account_manager)
 
 -- relasi job client
 alter table hr.employee_client_contract
