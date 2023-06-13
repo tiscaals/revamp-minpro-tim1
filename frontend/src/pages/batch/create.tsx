3300dce39fb7721e0fa3d689685e1aa9cf091a27
zufarhandito <zufarhandito@gmail.com>
2023-06-14 05:25:35 +0700

    untuk raihan

--- frontend/src/pages/batch/create.tsx
+++ frontend/src/pages/batch/create.tsx
@@ -63,17 +63,18 @@ const trainerData: any[] = [
 ];
 
 export default function Content() {
+  const {programs} = useSelector((state:any)=>state.programReducers)
+  const {trainers} = useSelector((state:any)=>state.trainerReducers)
+
   const [checked, setChecked] = useState<any>([]);
-  const [selectedTrainer, setSelectedTrainer] = useState(trainerData[0]);
-  const [selectedCoTrainer, setSelectedCoTrainer] = useState(trainerData[0]);
-  const [query, setQuery] = useState('');
+  const [selectedTrainer, setSelectedTrainer] = useState();
+  const [selectedCoTrainer, setSelectedCoTrainer] = useState();
   const [selTechno, setSelTechno] = useState<string>('')
   const [batchType, setBatchType] = useState<string>('')
 
-  const {trainers,programs} = useSelector((state:any)=>state.batchReducers)
   const dispatch = useDispatch()
   console.log(trainers);
-  // console.log(programs);
+
   const {
     register,
     handleSubmit,
@@ -83,7 +84,11 @@ export default function Content() {
   const onSubmit = (data: any) => {
     data.batch_entity_id = selTechno
     data.batch_type = batchType
-    data.trainee = checked.sort((a, b) => a - b)
+    data.batch_status = 'open'
+
+    //PIC diambil dari user yang login (recruiter)
+    data.batch_pic_id = 1
+    data.trainee = checked.sort((a:any, b:any) => a - b)
     data.instructors = [selectedTrainer,selectedCoTrainer]
     console.log(data)
   };
@@ -100,15 +105,6 @@ export default function Content() {
     dispatch(getAllTrainersReq())
     dispatch(getAllProgramsReq())
   },[])
-
-  const filteredPeople = trainers
-  // query === ''
-  //   ? trainers
-  //   : trainers.filter((trainer: any) => {
-  //       return trainer.user_first_name.toLowerCase().includes(query.toLowerCase());
-  //     });
-
-// console.log(trainers);
 
   return (
     <div className="w-full bg-white rounded-md p-10 mx-auto ">
@@ -166,94 +162,28 @@ export default function Content() {
             </div>
           </div>
         </div>
-
+        <br />
+          <Typography color="gray" className="font-normal mb-1 mt-3">
+              Select Trainer and Co-Trainer
+            </Typography>
         <div className="flex flex-col lg:flex-row gap-5 lg:gap-10 mt-5 ">
           <div className="lg:w-1/2">
-            <Typography color="black" className="font-normal mb-2 mt-3">
-              Trainer
-            </Typography>
-            <Select onChange={setSelectedTrainer} label="Type" >
+            <Select onChange={setSelectedTrainer} label="Trainer" >
                 {
                   (trainers || []).map((item:any)=> (
-                    <Option value={item.emp_entity_id}>{item.user_first_name}</Option>
+                    <Option value={item}>{item.user_first_name}</Option>
                   ))
                 }
-              </Select>
+            </Select>
           </div>
           <div className="lg:w-1/2">
-            <Typography color="black" className="font-normal mb-2 mt-3">
-              Co-Trainer
-            </Typography>
-            <Combobox value={selectedCoTrainer} onChange={setSelectedCoTrainer}>
-              <div className="relative mt-1">
-                <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                  <Combobox.Input
-                    className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:outline-none"
-                    displayValue={(trainer: any) => trainer.name}
-                    onChange={event => setQuery(event.target.value)}
-                  />
-                  <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                    <HiChevronDown
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </Combobox.Button>
-                </div>
-                <Transition
-                  as={Fragment}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                  afterLeave={() => setQuery('')}
-                >
-                  <Combobox.Options className="absolute mt-1 z-10 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {(filteredPeople || []).length === 0 && query !== '' ? (
-                      <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                        Nothing found.
-                      </div>
-                    ) : (
-                      (filteredPeople || []).map(trainer => (
-                        <Combobox.Option
-                          key={trainer.id}
-                          className={({ active }) =>
-                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                              active
-                                ? 'bg-light-blue-500 text-white'
-                                : 'text-gray-900'
-                            }`
-                          }
-                          value={trainer}
-                        >
-                          {({ selected, active }) => (
-                            <>
-                              <span
-                                className={`block truncate ${
-                                  selected ? 'font-medium' : 'font-normal'
-                                }`}
-                              >
-                                {trainer.name}
-                              </span>
-                              {selected ? (
-                                <span
-                                  className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                    active ? 'text-white' : 'text-teal-600'
-                                  }`}
-                                >
-                                  <HiCheck
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                              ) : null}
-                            </>
-                          )}
-                        </Combobox.Option>
-                      ))
-                    )}
-                  </Combobox.Options>
-                </Transition>
-              </div>
-            </Combobox>
+            <Select onChange={setSelectedCoTrainer} label="Co-Trainer" >
+                {
+                  (trainers || []).map((item:any)=> (
+                    <Option value={item}>{item.user_first_name}</Option>
+                  ))
+                }
+            </Select>
           </div>
         </div>
 
--- frontend/src/pages/redux/bootcamp-schema/reducer/batchReducer.tsx
+++ frontend/src/pages/redux/bootcamp-schema/reducer/batchReducer.tsx
@@ -4,9 +4,7 @@ const initialState = {
     batches: [],
     message: '',
     status: 0,
-    refresh: '',
-    programs: '',
-    trainers: ''
+    refresh: ''
 }
 
 function batchReducers(state = initialState, action: any) {
@@ -14,12 +12,6 @@ function batchReducers(state = initialState, action: any) {
     switch (type){
         case ActionTypes.RES_GET_BATCHES:
             return {state, batches: payload.data, refresh:true}
-
-        case ActionTypes.RES_GET_PROGRAMS:
-            return {state, programs: payload.data, refresh:true}
-
-        case ActionTypes.RES_GET_TRAINERS:
-            return {state, trainers: payload.data, refresh:true}
 
         case ActionTypes.RES_CREATE_BATCH:
             return {message:payload.message, status: payload.status, refresh:false}
