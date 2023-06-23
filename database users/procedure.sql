b01e9cabe148925c5a88b5792ab1946a2a04351f
bagasarya15 <bagasarya150800@gmail.com>
2023-06-23 18:39:06 +0700

    update procedure.sql

--- database users/procedure.sql
+++ database users/procedure.sql
@@ -65,32 +65,82 @@ END;
 $$;
 
 -- procedure update address
-CREATE OR REPLACE PROCEDURE users.update_address (
-    IN address_id INT,
-    IN first_address VARCHAR,
-    IN second_address VARCHAR,
-    IN code_pos VARCHAR,
-    IN city_id INT,
-    IN address_type_id INT
-)
-LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    UPDATE master.address
-    SET addr_line1 = first_address,
-        addr_line2 = second_address,
-        addr_postal_code = code_pos,
-        addr_city_id = city_id
-    WHERE addr_id = address_id;
-    
-    UPDATE users.users_address
-    SET etad_adty_id = address_type_id
-    WHERE etad_addr_id = address_id;
-END;
-$$;
+    CREATE OR REPLACE PROCEDURE users.update_address (
+        IN address_id INT,
+        IN first_address VARCHAR,
+        IN second_address VARCHAR,
+        IN code_pos VARCHAR,
+        IN city_id INT,
+        IN address_type_id INT
+    )
+    LANGUAGE plpgsql
+    AS
+    $$
+    BEGIN
+        UPDATE master.address
+        SET addr_line1 = first_address,
+            addr_line2 = second_address,
+            addr_postal_code = code_pos,
+            addr_city_id = city_id
+        WHERE addr_id = address_id;
+        
+        UPDATE users.users_address
+        SET etad_adty_id = address_type_id
+        WHERE etad_addr_id = address_id;
+    END;
+    $$;
 
 -- call untuk add address
 call users.add_address('Jalan Depok 1', 'Test', '16439', 1, 2, 1)
 -- call untuk update address
 call users.update_address(1, 'Jalan Depok', 'Test', '16439', 2, 1);
+
+-- Procedure Apply Jobs
+CREATE OR REPLACE PROCEDURE users.apply_jobs (
+    IN user_id INT,
+    IN firstname VARCHAR,
+    IN lastname VARCHAR,
+    IN user_school VARCHAR,
+    IN user_degree VARCHAR,
+    IN user_field_study VARCHAR,
+	IN user_phone_number VARCHAR,
+	IN filename VARCHAR,
+	IN filetype VARCHAR,
+	IN role_id INT
+)
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    update_role INT;
+BEGIN
+    UPDATE users.users_education
+    SET usdu_school = user_school,
+		usdu_degree = user_degree,
+		usdu_field_study = user_field_study
+    WHERE usdu_entity_id = user_id;
+	
+	UPDATE users.users_phones 
+	SET uspo_number = user_phone_number 
+	WHERE uspo_entity_id = user_id;
+	
+	UPDATE users.users_media
+	SET usme_filename = filename,
+		usme_filetype = filetype 
+	WHERE usme_entity_id = user_id;
+	
+	UPDATE users.users_roles
+	SET usro_role_id = role_id
+	WHERE usro_entity_id = user_id
+	RETURNING usro_role_id INTO update_role;
+	
+	UPDATE users.users
+    SET user_first_name = firstname,
+        user_last_name = lastname,
+		user_current_role = update_role
+    WHERE user_entity_id = user_id;
+END;
+$$;
+
+-- contoh call untuk aplly jobs
+call users.apply_jobs(9, 'hendri', 'prasmono', 'codex academy university', 'Diploma', 'd3 informatika', '08127', 'mycv.pdf', 'pdf', 7 )
